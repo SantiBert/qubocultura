f094fb0f819184481840625ebfe5f9ad4c2ad5d0
SantiBert <santibertero34@gmail.com>
2020-06-29 19:54:03 +0000

    primero desde produccion

--- qultura/configuracion/produccion.py
+++ qultura/configuracion/produccion.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['qubo.pythonanywhere.com','www.qubocultura.com']
+ALLOWED_HOSTS = ['qubo.pythonanywhere.com','www.qubocultura.com','qubocultura.com']
 
 
 DATABASES = {
